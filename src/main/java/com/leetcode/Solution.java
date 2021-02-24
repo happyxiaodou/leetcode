12b7cc696c97357f1fe4bc7a1e2cd48a188245a0
douhuatong <yfxiaodou@163.com>
2021-02-24 10:24:13 +0800

    反转链表

---
+++ src/main/java/com/leetcode/Solution.java
@@ -0,0 +1,81 @@
+package com.leetcode;
+
+/**
+ * 92. 反转链表 II
+ * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+ * <p>
+ * 说明:
+ * 1 ≤ m ≤ n ≤ 链表长度。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+ * 输出: 1->4->3->2->5->NULL
+ */
+public class Solution {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head.next == null) {
+            return head;
+        }
+        ListNode last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+
+    ListNode successor = null; // 后驱节点
+
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            // 相当于反转前 right 个元素
+            return reverseN(head, right);
+        }
+        head.next.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
+        Solution solution = new Solution();
+        listNode = solution.reverseN(listNode, 4);
+
+        while (listNode != null) {
+            System.err.println(listNode.val);
+            listNode = listNode.next;
+        }
+        System.err.println("");
+    }
+
+}
