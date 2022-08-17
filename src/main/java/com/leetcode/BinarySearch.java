90e53392dabee7cdc41a4149193155afb76ffd8b
douhuatong <yfxiaodou@163.com>
2022-08-17 13:19:55 +0800

    二次查找

---
+++ src/main/java/com/leetcode/BinarySearch.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+/**
+ * 704. 二分查找
+ * <p>
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ */
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+
+            int c = nums[mid];
+            if (c == target) {
+                return mid;
+            } else if (c > target) {
+                right = right - 1;
+            } else {
+                left = left + 1;
+            }
+
+        }
+        return -1;
+
+    }
+}
